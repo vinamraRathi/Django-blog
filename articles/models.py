a87a4c277ea3036db0cfde90f794fde67dbd1f9c
vinamraRathi <vinamrarathi771@gmail.com>
2019-07-08 18:27:51 +0530

    First Commit

---
+++ articles/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    #add in thumbnail later
+    # add in author later
+
+    def __str__(self):
+        return self.title
+
+    def snippet(self):
+        return self.body[:150] + ' ...(to be continued)'
