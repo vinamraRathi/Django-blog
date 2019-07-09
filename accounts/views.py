021fda9cee874651b77e4d32c9b2649af4cf5c92
vinamraRathi <vinamrarathi771@gmail.com>
2019-07-09 15:07:24 +0530

    Updating Authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
             #log the user in
+            login(request, user)
             return redirect('articles:list')
     else:
         form = UserCreationForm()
@@ -17,7 +19,17 @@ def login_view(request):
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             #login the user
-            return redirect('articles:list')
+            user = form.get_user()
+            login(request, user)
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+                return redirect('articles:list')
     else:
         form = AuthenticationForm()
     return render(request, 'accounts/login.html', {'form':form})
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('articles:list')
